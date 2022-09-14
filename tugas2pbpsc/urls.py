7ed05d863b4c3faa3b8fab67183b51922f7468d6
Stelline Claudia <stellineclaudia32@gmail.com>
2022-09-14 11:03:50 +0700

    test10

--- tugas2pbpsc/urls.py
+++ tugas2pbpsc/urls.py
@@ -17,8 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('example_app.urls')),
     path('katalog/', include('katalog.urls')),
 ]
 
