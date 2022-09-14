e8c0bb716efa62a655527abd7694670304028dd2
Stelline Claudia <stellineclaudia32@gmail.com>
2022-09-14 10:16:45 +0700

    test3

--- project_django/settings.py
+++ project_django/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'stelline.urls'
+ROOT_URLCONF = 'project_django.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'stelline.wsgi.application'
+WSGI_APPLICATION = 'project_django.wsgi.application'
 
 
 # Database
