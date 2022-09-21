fdb67ec9b2f4c74c8cc430957d3c6d2464e204df
Stelline Claudia <stellineclaudia32@gmail.com>
2022-09-21 11:16:37 +0700

    test1

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -6,11 +6,11 @@ from mywatchlist.views import show_json
 app_name = 'mywatchlist'
 
 urlpatterns = [
-    path('html', show_watchlist, name='show_watchlist'),
+    path('html/', show_watchlist, name='show_watchlist'),
    
-    path('xml', show_xml, name='show_xml'), 
+    path('xml/', show_xml, name='show_xml'), 
 
-    path('json', show_json, name='show_json'),
+    path('json/', show_json, name='show_json'),
 
 
 ]
