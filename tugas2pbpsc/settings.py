b49f86e9dac6aff5a590ff039bc9c4f3e24d7176
Stelline Claudia <stellineclaudia32@gmail.com>
2022-10-13 01:31:21 +0700

    test2

--- tugas2pbpsc/settings.py
+++ tugas2pbpsc/settings.py
@@ -15,6 +15,7 @@ import dj_database_url
 from pathlib import Path
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = 'static'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
